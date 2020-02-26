e7867d6ef326ceefe4fd25ad9e5f5ea91d52bae4
Unigames <uwaunigames@gmail.com>
2020-02-26 14:15:34 +0800

    altered admin for member details

--- members/admin.py
+++ members/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Member, Membership, Rank
-admin.site.register(Member)
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('preferred_name', 'last_name', 'join_date', 'is_fresher')
+
+
+admin.site.register(Member, MemberAdmin)
 admin.site.register(Membership)
 admin.site.register(Rank)
