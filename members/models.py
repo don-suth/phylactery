f0d59dc9549dfef3c7590bcfa4c9ef0f6ea6dab2
Unigames <uwaunigames@gmail.com>
2020-02-26 14:03:14 +0800

    add join_date and is_fresher

--- members/models.py
+++ members/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Member(models.Model):
@@ -8,11 +10,15 @@ class Member(models.Model):
 	pronouns = models.CharField(max_length=200)
 	email_address = models.CharField(max_length=200)
 	student_number = models.CharField(max_length=10)
+	join_date = models.DateField(auto_now_add=True)
 	notes = models.TextField()
-	
+
 	def __str__(self):
-		return self.preferred_name + ' ' + self.last_name + ' (' + self.pronouns + ')'
-	
+		return self.preferred_name + ' ' + self.last_name
+
+	def is_fresher(self):
+		return self.join_date.year == timezone.now().year
+
 class Membership(models.Model):
 	member = models.ForeignKey(Member, on_delete=models.CASCADE)
 	date = models.DateTimeField()
