ae09f17ccf68b7165542898d7f0fb876ec3a8195
Unigames <uwaunigames@gmail.com>
2020-02-27 16:24:24 +0800

    Heavily updated admin interface + models with default preferred name

--- members/admin.py
+++ members/admin.py
@@ -3,10 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Member, Membership, Rank
 
+class RanksInline(admin.TabularInline):
+    model = Rank.member.through
+    extra = 1
+
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 1
+
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'pronouns', 'email_address', 'student_number', 'join_date', 'is_fresher', 'notes')
+    inlines = [MembershipInline, RanksInline]
+
+class RanksAdmin(admin.ModelAdmin):
+    inlines = [RanksInline]
+    exclude = ('member',)
 
 
 admin.site.register(Member, MemberAdmin)
-admin.site.register(Membership)
-admin.site.register(Rank)
+admin.site.register(Rank, RanksAdmin)
--- members/models.py
+++ members/models.py
@@ -6,12 +6,18 @@ from django.utils import timezone
 class Member(models.Model):
 	first_name = models.CharField(max_length=200)
 	last_name = models.CharField(max_length=200)
-	preferred_name = models.CharField(max_length=200)
+	preferred_name = models.CharField(max_length=200, blank=True)
 	pronouns = models.CharField(max_length=200)
 	email_address = models.CharField(max_length=200)
 	student_number = models.CharField(max_length=10)
-	join_date = models.DateField(auto_now_add=True)
-	notes = models.TextField()
+	join_date = models.DateField(default=timezone.now)
+	notes = models.TextField(blank=True)
+
+	def save(self, *args, **kwargs):
+		if self.preferred_name == "":
+			self.preferred_name = self.first_name
+		super(Member, self).save(*args, **kwargs)
+
 
 	def __str__(self):
 		return self.preferred_name + ' ' + self.last_name
@@ -25,5 +31,19 @@ class Membership(models.Model):
 	guild_member = models.BooleanField()
 	
 class Rank(models.Model):
-	rank_name = models.CharField(max_length=50)
-	member = models.ManyToManyField(Member)
+	RANK_CHOICES = [
+		('SUSPENDED', 'Suspended'),
+		('EXPELLED', 'Expelled'),
+		('GATEKEEPER', 'Gatekeeper'),
+		('WEBKEEPER', 'Webkeeper'),
+		('COMMITTEE', 'Committee'),
+		('LIFEMEMBER', 'Life Member')
+	]
+	rank_name = models.CharField(
+		max_length=20,
+		choices=RANK_CHOICES
+	)
+	member = models.ManyToManyField(Member, related_name='ranks')
+
+	def __str__(self):
+		return self.rank_name
