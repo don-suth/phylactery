3a29c7d96bc440f6e9481a56abc3c7a16fd83a13
dragonxdoom <donald@sutherland.id.au>
2020-08-28 18:48:37 +0800

    Update admin interface for tags

--- library/admin.py
+++ library/admin.py
@@ -4,8 +4,24 @@ from django.contrib import admin
 from .models import Item, ItemTypes, StrTag, StrTagValues, IntTagValues, IntTag, StaticTag
 
 
+class StrTagValuesInline(admin.TabularInline):
+    model = StrTagValues
+    extra = 1
+
+
+class IntTagValuesInline(admin.TabularInline):
+    model = IntTagValues
+    extra = 1
+
+
+class StaticTagInline(admin.TabularInline):
+    model = StaticTag.items.through
+    extra = 1
+
+
 class ItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {"item_slug": ("item_name",)}
+    inlines = (StrTagValuesInline, IntTagValuesInline, StaticTagInline)
 
 
 admin.site.register(Item, ItemAdmin)
