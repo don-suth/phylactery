0814bc3ca1b124233cd82dd95e2e3974d1f4b949
DragonXDoom <donald@sutherland.id.au>
2020-08-30 15:23:02 +0800

    Add Select2 fields to Tag M2M fields

--- library/admin.py
+++ library/admin.py
@@ -4,18 +4,33 @@ from django.contrib import admin
 from .models import Item, ItemTypes, StrTag, StrTagValues, IntTagValues, IntTag, StaticTag
 
 
+class StrTagAdmin(admin.ModelAdmin):
+    search_fields = ('tag_name',)
+
+
+class IntTagAdmin(admin.ModelAdmin):
+    search_fields = ('tag_name',)
+
+
+class StaticTagAdmin(admin.ModelAdmin):
+    search_fields = ('tag_name',)
+
+
 class StrTagValuesInline(admin.TabularInline):
     model = StrTagValues
+    autocomplete_fields = ('tag',)
     extra = 1
 
 
 class IntTagValuesInline(admin.TabularInline):
     model = IntTagValues
+    autocomplete_fields = ('tag',)
     extra = 1
 
 
 class StaticTagInline(admin.TabularInline):
     model = StaticTag.items.through
+    autocomplete_fields = ('statictag',)
     extra = 1
 
 
@@ -26,6 +41,6 @@ class ItemAdmin(admin.ModelAdmin):
 
 admin.site.register(Item, ItemAdmin)
 admin.site.register(ItemTypes)
-admin.site.register(StrTag)
-admin.site.register(IntTag)
-admin.site.register(StaticTag)
+admin.site.register(StrTag, StrTagAdmin)
+admin.site.register(IntTag, IntTagAdmin)
+admin.site.register(StaticTag, StaticTagAdmin)
