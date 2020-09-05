488f34381b14e2332d5ca0e236a8c571a007d518
dragonxdoom <donald@sutherland.id.au>
2020-09-05 23:13:05 +0800

    Add item_list_view and support

--- library/urls.py
+++ library/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'library'
 urlpatterns = [
     path('item/<int:item_id>/', views.item_detail, name='detail-id'),
-    path('item/<slug:slug>/', views.item_detail, name='detail-slug')
+    path('item/<slug:slug>/', views.item_detail, name='detail-slug'),
+    path('', views.item_list, name='items-all')
 ]
--- library/views.py
+++ library/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
+from django.http import HttpResponseBadRequest
 
 
 # Create your views here.
@@ -9,4 +10,13 @@ def item_detail(request, item_id=None, slug=None):
         return redirect(item)
     elif slug is not None:
         item = get_object_or_404(Item, item_slug=slug)
-    return render(request, 'library/item_view.html', {'item': item})
+    else:
+        return HttpResponseBadRequest("Invalid request")
+    return render(request, 'library/item_detail_view.html', {'item': item})
+
+
+def item_list(request, page=1):
+    # Shows a list of all items, sorted alphabetically, in pages of 10
+    # 0-9, 10-19, etc.
+    items_list = Item.objects.order_by('item_name')[(page-1)*10:(page*10)-1]
+    return render(request, 'library/item_list_view.html', {'items_list': items_list})
