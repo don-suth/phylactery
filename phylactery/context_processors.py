388263b0b8277f506263f22ec9afd7c41c670ed2
donsuth <donald@sutherland.id.au>
2021-01-05 21:29:51 +0800

    add a context processor so templates get UnigamesUser

--- phylactery/context_processors.py
+++ phylactery/context_processors.py
@@ -2,9 +2,21 @@
 # Our use for it is enabling easy customisation of the menu bar for the changing dragon theme
 
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from members.models import switch_to_proxy
 
-def admin_colour_settings(request):
+
+def navbar_colour_settings(request):
     return {
         'NAVBAR_THEME': settings.NAVBAR_THEME,
         'NAVBAR_COLOUR': settings.NAVBAR_COLOUR
     }
+
+
+def unigames_user_processor(request):
+    if request.user.is_authenticated:
+        new_user = switch_to_proxy(request.user)
+        return {
+            'user': new_user
+        }
+    return {}
