eb246f80917d1b9440c3409044f06ffa2749816e
dragonxdoom <donald@sutherland.id.au>
2021-01-09 16:56:33 +0800

    Started to add tests, not finished yet though

--- library/tests.py
+++ library/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
-from .models import Item, ItemTypes
+from .models import Item, ItemTypes, BorrowRecord, ExternalBorrowingRecord
+from members.models import Member
+from django.utils import timezone
+import datetime
+from freezegun import freeze_time
 
 
 '''
@@ -18,8 +22,28 @@ Tests to make:
 
 class LibraryModelTests(TestCase):
     def setUp(self):
-        book_type = ItemTypes.objects.create(type_name="Book")
+        ItemTypes.objects.create(type_name="Book")
         Item.objects.create(
             item_name="Dungeons & Dragons Players Handbook 5th Edition",
-            item_type=book_type,
+            item_type=ItemTypes.objects.get(type_name='Book'),
+        )
+        Member.objects.create(
+            first_name='Donald',
+            last_name='Sutherland',
+            email_address='donald@sutherland.id.au',
+        )
+
+    @freeze_time("9th January 2021")
+    def test_availability_info(self):
+        today = timezone.now()
+        self.assetEqual(today, datetime.date(2021, 1, 9))
+
+        currently_borrowed = BorrowRecord.objects.create(
+            borrowing_member=Member.objects.get(first_name='Donald'),
+            member_address='123 Fake Avenue, 9999',
+            member_phone_number='0123456789',
+            item=Item.objects.get(pk=1),
+            date_borrowed=today - datetime.timedelta(days=1),
+            auth_gatekeeper=Member.objects.get(first_name='Donald'),
+            due_date=today + datetime.timedelta(days=1)
         )
