6519168d1ce7572461229a619cf33345bd5e8fd1
donsuth <donald@sutherland.id.au>
2021-01-09 14:45:38 +0800

    Added outline of what tests I should write

--- library/tests.py
+++ library/tests.py
@@ -2,32 +2,24 @@ from django.test import TestCase
 from .models import Item, ItemTypes
 
 
+'''
+Tests to make:
+    - item tagging
+    - searching items by tag (when implemented)
+    - searching in general (when implemented)
+    - hierarchical tags on item save (when implemented)
+    - hierarchical tags on force refresh (when implemented)
+    - borrow item, check if available
+    - borrow item, check availability date
+    - return item, check if available
+    - borrow item, with external form, check availability date
+'''
+
+
 class LibraryModelTests(TestCase):
     def setUp(self):
-        ItemTypes.objects.create(type_name="Book")
-        ItemTypes.objects.create(type_name="Board Game")
+        book_type = ItemTypes.objects.create(type_name="Book")
         Item.objects.create(
             item_name="Dungeons & Dragons Players Handbook 5th Edition",
-            item_type=ItemTypes.objects.get(type_name="Book"),
+            item_type=book_type,
         )
-        Item.objects.create(
-            item_name="Dungeons & Dragons Monster Manual 5th Edition",
-            item_type=ItemTypes.objects.get(type_name="Book"),
-        )
-        Item.objects.create(
-            item_name="Galaxy Truckers",
-            item_type=ItemTypes.objects.get(type_name="Board Game"),
-        )
-
-    def test_fetch_all_books(self):
-        qs = Item.objects.filter(item_type__type_name="Book")
-        self.assertQuerysetEqual(qs,
-                                 ['<Item: Dungeons & Dragons Players Handbook 5th Edition>',
-                                  '<Item: Dungeons & Dragons Monster Manual 5th Edition>'],
-                                 ordered=False)
-
-    def test_fetch_all_board_games(self):
-        qs = Item.objects.filter(item_type__type_name="Board Game")
-        self.assertQuerysetEqual(qs,
-                                 ['<Item: Galaxy Truckers>'],
-                                 ordered=False)
