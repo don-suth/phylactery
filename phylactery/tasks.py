9cc13dc4bde1a06a14a1a8f13da9a58897221713
donsuth <donald@sutherland.id.au>
2021-05-30 15:41:29 +0800

    Add helper function for composing HTML emails

--- phylactery/tasks.py
+++ phylactery/tasks.py
@@ -1,10 +1,28 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
-from django.core.mail import EmailMessage, get_connection
+from django.core.mail import get_connection
 from django.core.mail import send_mail, send_mass_mail
 
+from django.template.loader import render_to_string
+
+from django.utils.html import strip_tags
+
+from premailer import transform
+
 logger = get_task_logger(__name__)
+
+
+def compose_html_email(template_name, context):
+    """
+        Returns an email template, ready-to-email, in both plaintext and html form.
+    """
+    html_message = render_to_string(template_name, context)
+    html_message = transform(html_message)
+    context['override_base'] = 'phylactery/email_base.txt'
+    plaintext_message = render_to_string(template_name, context)
+    plaintext_message = strip_tags(plaintext_message)
+    return plaintext_message, html_message
 
 
 @shared_task(name="send_single_email_task")
