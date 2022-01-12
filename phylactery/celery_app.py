c0a10ca94c4f16c779afcb7649d22dd3a00de354
Donald Sutherland <donald@sutherland.id.au>
2022-01-12 10:24:11 +0000

    Update celery stuff

--- phylactery/celery_app.py
+++ phylactery/celery_app.py
@@ -1,12 +1,13 @@
 import os
 from celery import Celery
+from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phylactery.settings')
 app = Celery('phylactery')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
+app.config_from_object(settings)
 app.autodiscover_tasks()
 
 
