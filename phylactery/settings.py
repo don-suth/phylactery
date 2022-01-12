b8ae2a6937a045097154099f4c6decdb55f286ff
Donald Sutherland <donald@sutherland.id.au>
2022-01-12 21:20:55 +0800

    Update broker url to work with the new Celery version

--- phylactery/settings.py
+++ phylactery/settings.py
@@ -188,7 +188,7 @@ MARKDOWNIFY_WHITELIST_TAGS = [
 ]
 
 # Celery Stuff
-BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
