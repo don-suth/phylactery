22dc251d2a64ed504e67fd0d2cab2a1bb0ccb3ab
Donald Sutherland <donald@sutherland.id.au>
2022-01-20 22:24:23 +0800

    Add monospace font for blog post editing

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
+from django.forms import ModelForm
 
 # Register your models here.
 from .models import BlogPost, EmailOrder
 
 
+class BlogPostAdminForm(ModelForm):
+    model = BlogPost
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['body'].widget.attrs.update({'style': 'font-family: monospace, monospace;'})
+
+
 class BlogPostAdmin(admin.ModelAdmin):
     model = BlogPost
+    form = BlogPostAdminForm
     prepopulated_fields = {'slug_title': ('title',)}
 
 
