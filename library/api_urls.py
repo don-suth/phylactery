17743e8ae72a84e7f62f281a546926369b9a0ca6
donsuth <donald@sutherland.id.au>
2023-04-07 23:02:58 +0800

    Remove the 'all_items' API endpoint for the moment

--- library/api_urls.py
+++ library/api_urls.py
@@ -3,7 +3,7 @@ from . import api_viewsets
 app_name = 'library'
 
 urlpatterns = [
-	path('all', api_viewsets.ItemViewSet.as_view({'get': 'all_items'})),
+	#path('all', api_viewsets.ItemViewSet.as_view({'get': 'all_items'})),
 	path('random/any', api_viewsets.ItemViewSet.as_view({'get': 'random_item'})),
 	path('random/book', api_viewsets.ItemViewSet.as_view({'get': 'random_book'})),
 	path('random/boardgame', api_viewsets.ItemViewSet.as_view({'get': 'random_boardgame'})),
